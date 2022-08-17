ef36e043adc89ae8fa640610822a31169d625efc
Apungu Raymond <apunguray@gmail.com>
2022-08-17 20:32:07 +0800

    setup navigation

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { Text, View } from 'react-native';
-import { TailwindProvider } from 'tailwindcss-react-native';
+import { Text, View } from "react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen, Onboarding, TransactionDetail } from "./screens";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <TailwindProvider>
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Onboarding"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Onboarding" component={Onboarding} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen
+            name="TransactionDetail"
+            component={TransactionDetail}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </TailwindProvider>
   );
 }

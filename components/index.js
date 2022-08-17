71178d6dc906e0da63f3c41a1b058342a03e163a
Apungu Raymond <apunguray@gmail.com>
2022-08-18 00:38:58 +0800

    local update

--- components/index.js
+++ components/index.js
@@ -2,10 +2,12 @@ import Header from './Header';
 import RenderCardItem from './RenderCardItem';
 import FeaturedRow from './FeaturedRow'
 import RenderTranscation from './RenderTranscation'
+import RenderOverview from './RenderOverview'
 
 export {
     Header,
     RenderCardItem,
     FeaturedRow,
-    RenderTranscation
+    RenderTranscation,
+    RenderOverview
 }

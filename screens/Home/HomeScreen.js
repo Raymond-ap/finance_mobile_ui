417606a6467cf766b007ad1f1f05751516bed802
Apungu Raymond <apunguray@gmail.com>
2022-08-17 23:07:41 +0800

    commits

--- screens/Home/HomeScreen.js
+++ screens/Home/HomeScreen.js
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { useNavigation } from "@react-navigation/native";
+import { Header, RenderCardItem, FeaturedRow, RenderTranscation } from "../../components";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+    <SafeAreaView style={{ flex: 1 }} className="bg-black">
+      <Header />
+      <RenderCardItem />
+      {/* <FeaturedRow label={"overview"} /> */}
+      <FeaturedRow label={"latest transaction"} render={<RenderTranscation/>} />
+      <StatusBar style="light" translucent={false} backgroundColor="#000" />
+    </SafeAreaView>
+  );
 }

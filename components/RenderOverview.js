3f4cc23f5c940f4657b3ed24bea4fc58ce75f94f
Apungu Raymond <apunguray@gmail.com>
2022-08-18 00:38:49 +0800

    local update

---
+++ components/RenderOverview.js
@@ -0,0 +1,53 @@
+import { View, Text, Dimensions, FlatList } from "react-native";
+import React from "react";
+import { Overview } from "../constants";
+import { Feather } from "@expo/vector-icons";
+
+export default function RenderOverview() {
+  return (
+    <View
+      className="py-2"
+      style={{
+        width: Dimensions.get("screen").width,
+      }}
+    >
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={Overview}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({ item }) => <Card item={item} />}
+      />
+    </View>
+  );
+}
+
+const Card = ({ item }) => {
+  return (
+    <View className="bg-neutral-800 rounded-lg w-56 shadow-lg mx-1 py-5 px-4">
+      <Text className="text-gray-300 text-lg font-bold capitalize tracking-wider">
+        {item.label}
+      </Text>
+      <Text className="mt-5 text-white text-xl font-bold capitalize tracking-widest">
+        {item.amount}
+      </Text>
+      <View className="flex flex-row items-center justify-between mt-2">
+        <View className="flex flex-row items-center">
+          <Feather
+            name={item.type === "inc" ? "arrow-up-right" : "arrow-down-right"}
+            color={item.type === "inc" ? "#059669" : "#dc2626"}
+            size={15}
+          />
+          <Text
+            className={`ml-1 text-sm font-bold ${
+              item.type === "inc" ? "text-green-600" : "text-red-600"
+            }`}
+          >
+            {item.percentage}
+          </Text>
+        </View>
+        <Text className={`ml-1 text-sm text-gray-300`}>{item.date}</Text>
+      </View>
+    </View>
+  );
+};

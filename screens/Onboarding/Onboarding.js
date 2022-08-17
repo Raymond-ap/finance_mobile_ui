13072877a12d0ba823775afea529f05ef62cedeb
Apungu Raymond <apunguray@gmail.com>
2022-08-17 20:55:23 +0800

    local update

--- screens/Onboarding/Onboarding.js
+++ screens/Onboarding/Onboarding.js
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Onboarding() {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>Onboarding</Text>
-    </View>
-  )
+    <SafeAreaView className="bg-black" style={{ flex: 1 }}>
+      <View className="px-5 w-full flex justify-end h-full pb-5">
+        <Text className="text-white w-56 capitalize leading-10 font-bold text-2xl tracking-wider mb-2">
+          Manage your finances with a simple card
+        </Text>
+        <Text className="text-gray-300 text-base">
+          Have this card and start managing your daily expenses to get a more
+          organized future.
+        </Text>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Home")}
+          className="mt-10 w-full bg-green-100 py-4 justify-center items-center rounded-md shadow-md"
+          activeOpacity={0.8}
+        >
+          <Text className="text-base capitalize tracking-wider ">
+            get started now
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <StatusBar style="light" translucent={false} backgroundColor="#000" />
+    </SafeAreaView>
+  );
 }

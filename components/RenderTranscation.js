69d5010689b3bba43eee2c24748148cdf91b8004
Apungu Raymond <apunguray@gmail.com>
2022-08-17 23:07:09 +0800

    initial commit

---
+++ components/RenderTranscation.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function RenderTranscation() {
+  return (
+    <View className="px-4">
+      <Text className="text-white">RenderTranscation</Text>
+    </View>
+  )
+}

90d74a6efb4e1e28a5c9e1e3b6fec35875882fe5
Apungu Raymond <apunguray@gmail.com>
2022-08-17 20:31:51 +0800

    global exports

---
+++ screens/index.js
@@ -0,0 +1,9 @@
+import HomeScreen from "./Home/HomeScreen";
+import Onboarding from "./Onboarding/Onboarding";
+import TransactionDetail from "./Detail/TransactionDetail"; 
+
+export {
+    HomeScreen,
+    Onboarding,
+    TransactionDetail
+}

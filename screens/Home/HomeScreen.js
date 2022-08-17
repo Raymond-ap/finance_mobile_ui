ed1b8bc822b4dc23a84b10fefc37b3c369d5da1b
Apungu Raymond <apunguray@gmail.com>
2022-08-18 00:20:21 +0800

    local update

--- screens/Home/HomeScreen.js
+++ screens/Home/HomeScreen.js
@@ -1,16 +1,32 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { useNavigation } from "@react-navigation/native";
-import { Header, RenderCardItem, FeaturedRow, RenderTranscation } from "../../components";
+import {
+  Header,
+  RenderCardItem,
+  FeaturedRow,
+  RenderTranscation,
+} from "../../components";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={{ flex: 1 }} className="bg-black">
       <Header />
-      <RenderCardItem />
-      {/* <FeaturedRow label={"overview"} /> */}
-      <FeaturedRow label={"latest transaction"} render={<RenderTranscation/>} />
+      <ScrollView>
+        <RenderCardItem />
+        {/* <FeaturedRow label={"overview"} /> */}
+        <FeaturedRow
+          label={"latest transaction"}
+          render={<RenderTranscation />}
+        />
+      </ScrollView>
       <StatusBar style="light" translucent={false} backgroundColor="#000" />
     </SafeAreaView>
   );

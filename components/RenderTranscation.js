7f6b7bf5d3a5b4ce4d6ae7c369837ebe4aecf758
Apungu Raymond <apunguray@gmail.com>
2022-08-18 00:20:24 +0800

    local update

--- components/RenderTranscation.js
+++ components/RenderTranscation.js
@@ -1,10 +1,67 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { Transactions } from "../constants";
+import {
+  Ionicons,
+  MaterialCommunityIcons,
+  FontAwesome,
+  Fontisto,
+  FontAwesome5Brands,
+} from "@expo/vector-icons";
 
 export default function RenderTranscation() {
   return (
-    <View className="px-4">
-      <Text className="text-white">RenderTranscation</Text>
+    <View className="mx-4">
+      {Transactions.map((item, index) => {
+        console.log("item.vectorIcon", item.vectorIcon);
+        return (
+          <View
+            className="my-2 flex flex-row py-3 items-center justify-between"
+            key={index}
+          >
+            <View
+              className={`w-12 justify-center items-center h-12 rounded-full ${
+                index % 3 === 1 ? "bg-teal-200" : "bg-neutral-800"
+              }`}
+            >
+              <FontAwesome
+                name={item.vectorName}
+                size={20}
+                color={index % 3 === 1 ? "#262626" : "#ccfbf1"}
+              />
+            </View>
+            <View style={{ flex: 1 }} className="mx-3">
+              <View className="flex flex-row items-center">
+                <Text className="text-white font-bold capitalize text-base tracking-wider">
+                  {item.title}
+                </Text>
+                {item.accountType && (
+                  <Text className="bg-amber-200 text-black px-1 py-0.5 mx-1 rounded-lg capitalize text-xs tracking-wider">
+                    {item.accountType}
+                  </Text>
+                )}
+              </View>
+              <Text className="text-gray-200 capitalize text-sm tracking-wider">
+                {item.date}
+              </Text>
+            </View>
+            <View className="flex flex-row items-center">
+              <MaterialCommunityIcons
+                name={item.type === "inc" ? "plus" : "minus"}
+                size={15}
+                color={item.type === "inc" ? "#059669" : "#dc2626"}
+              />
+              <Text
+                className={`text-sm font-bold ${
+                  item.type === "inc" ? "text-green-600" : "text-red-600"
+                }`}
+              >
+                {item.amount}
+              </Text>
+            </View>
+          </View>
+        );
+      })}
     </View>
-  )
+  );
 }

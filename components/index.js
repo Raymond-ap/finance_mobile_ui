379b00ffad4d8f0b25ee9256d5d401c8bc93ba9a
Apungu Raymond <apunguray@gmail.com>
2022-08-17 23:06:36 +0800

    local update

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+import RenderCardItem from './RenderCardItem';
+import FeaturedRow from './FeaturedRow'
+import RenderTranscation from './RenderTranscation'
+
+export {
+    Header,
+    RenderCardItem,
+    FeaturedRow,
+    RenderTranscation
+}
